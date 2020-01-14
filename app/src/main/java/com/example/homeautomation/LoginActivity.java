a8cae2b3966a06daf173cbd00944b34d795a096d
basit96 <pakkobra007@gmail.com>
2020-01-14 12:08:45 +0500

    fan + sensors + login

--- app/src/main/java/com/example/homeautomation/LoginActivity.java
+++ app/src/main/java/com/example/homeautomation/LoginActivity.java
@@ -2,14 +2,39 @@ package com.example.homeautomation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btn;
+    EditText id,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn=(Button) findViewById(R.id.login);
+        id=(EditText) findViewById(R.id.id);
+        password=(EditText) findViewById(R.id.password);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (id.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                } else if (!password.getText().toString().equals("admin")) {
+                    password.setError("Invalid password");
+                    password.setFocusable(true);
+                } else {
+                    id.setError("Invalid id");
+                    id.setFocusable(true);
+                }
+            }
+            }
+        );
 
     }
 }
